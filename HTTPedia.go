6c38c480989a9918d1994d66e06966364b091229
ryan nemeth <ryannemeth@live.com>
2023-12-28 16:29:10 -0500

    init

--- HTTPedia.go
+++ HTTPedia.go
@@ -1,1 +1,45 @@
 package HTTPedia
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type StatusCode struct {
+	Code        int    `json:"code"`
+	Description string `json:"description"`
+}
+
+func GetStatusCode(code int) (string, error) {
+	statusCodesFromFile, err := parseJson("status_codes.json")
+	if err != nil {
+		return "", err
+	}
+
+	returnValue := StatusCode{}
+
+	if len(statusCodesFromFile) > 0 {
+		for _, statusCode := range statusCodesFromFile {
+			if statusCode.Code == code {
+				returnValue = statusCode
+			}
+		}
+	}
+
+	return returnValue.Description, nil
+}
+
+func parseJson(filename string) ([]StatusCode, error) {
+	statusCodes := []StatusCode{}
+
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := json.Unmarshal(file, &statusCodes); err != nil {
+		return nil, err
+	}
+
+	return statusCodes, nil
+}

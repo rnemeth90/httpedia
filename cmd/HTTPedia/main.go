99c57ce01f4331a02697c9e4e7cbbddb1097d712
ryan nemeth <ryannemeth@live.com>
2024-01-02 16:03:44 -0500

    updates

--- cmd/HTTPedia/main.go
+++ cmd/HTTPedia/main.go
@@ -1,5 +1,56 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"os"
 
+	"github.com/rnemeth90/HTTPedia"
+	"github.com/spf13/pflag"
+)
+
+var (
+	statusCode int
+)
+
+type config struct {
+	statusCode int
+}
+
+func init() {
+	pflag.IntVarP(&statusCode, "statuscode", "s", 0, "Status Code")
+	pflag.Usage = usage
+}
+
+func usage() {
+	pflag.PrintDefaults()
+}
+
+func main() {
+	pflag.Parse()
+
+	c := config{
+		statusCode: statusCode,
+	}
+
+	result, err := run(c)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+	}
+
+	jsonStr, err := json.MarshalIndent(result, "", "  ")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+	}
+
+	fmt.Fprintln(os.Stdout, jsonStr)
+}
+
+func run(c config) (string, error) {
+	description, err := HTTPedia.GetStatusCode(c.statusCode)
+	if err != nil {
+		return "", err
+	}
+
+	return description, nil
 }
